36eea90c61ac76627cac9744b0fec093308a3c7b
MiliRamirezLarre <131774533+MiliRamirezLarre@users.noreply.github.com>
2023-09-12 22:29:49 -0400

    calculadora basal funcional

--- script.js
+++ script.js
@@ -1,44 +1,64 @@
 const CALCULAR = document.getElementById('btn-calcular');
 const ERROR = document.getElementById('error');
 const FLU = document.getElementById('flu');
+const VOL = document.getElementById('vol');
 const MAN = document.getElementById('man');
+const SC1 = document.getElementById('sc1');
+const SC2 = document.getElementById('sc2');
 
 CALCULAR.addEventListener('click', () => {
     const DATO = document.getElementById('peso').value
     //validamos que se cargue un dato:
     if (DATO > 0){
-        ERROR.style.display = 'none'
-        let flujo = calcFlujo(DATO);
-        let mantenimiento = flujo*1.5;
-        FLU.innerHTML = flujo + ' cc/hr';
-        MAN.innerHTML = 'm+m/2 ' + mantenimiento + ' cc/hr';
-        FLU.style.display = 'block';
-        MAN.style.display = 'block';
+        ERROR.style.display = 'none';
+        let volDiario = calcVolumenDiario(DATO);
+       
+        // metodo Holliday segar
+        if(DATO <= 30) {
+            let mantenimiento = volDiario/24;
+            VOL.innerHTML = 'Volumen Diario ' + volDiario;
+            FLU.innerHTML = 'm + m/2 ' + mantenimiento*1.5 + ' cc/hr';
+            MAN.innerHTML = 'Mantenimiento ' + mantenimiento + ' cc/hr';
+
+            FLU.style.display = 'block';
+            VOL.style.display = 'block';
+            MAN.style.display = 'block';
+
+            SC1.style.display = 'none';
+            SC2.style.display = 'none';
+
+        }else { // metodo superficie corporal
+            let datoInt = parseInt(DATO);
+            let sc = ((datoInt * 4) + 7) / (datoInt + 90);
+            SC1.innerHTML = 'SC * 1500 ' + sc*1500;
+            SC2.innerHTML = 'SC * 2000 ' + sc*2000;
+
+            SC1.style.display = 'block';
+            SC2.style.display = 'block';
+
+            FLU.style.display = 'none';
+            VOL.style.display = 'none';
+            MAN.style.display = 'none';
+        }
     } else {
         ERROR.style.display = 'block';
         FLU.style.display = 'none';
+        VOL.style.display = 'none';
         MAN.style.display = 'none';
+        SC1.style.display = 'none';
+        SC2.style.display = 'none';
     }
 })
 
-function calcFlujo(peso){
+function calcVolumenDiario(peso){
     let resultado = 0;
-    if (peso <=30){
-        if (peso<=10){
-            resultado = peso*100;
-        }
-        if (peso>10 && peso<=20){
-            resultado = 1000+(resultado + ((peso-10)*50));
-        }
-        if (peso >20 && peso<=30){
-            resultado = 1500+(resultado + ((peso-20)*20));
-        }
-    }else{
-        let corporal = ( (peso * 4) + 7) / (peso + 90);
-        let sup1 = corporal * 1500;
-        let sup2 = corporal * 2000;
+    if(peso<=10){
+        resultado = peso*100;
+    }else if(peso > 10 && peso <= 20){
+        resultado = 1000 + ((peso-10) * 50);
+    }else if(peso> 20 && peso <= 30){
+        resultado = 1500 + ((peso -20) * 20);
     }
-    console.log(resultado);
     return resultado;
 }
 
